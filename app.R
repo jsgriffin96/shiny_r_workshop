36cba9b2e639f0b2a5055e703ed03e56bf05d9ab
jsgriffin96 <jsgriffin96@live.com>
2024-11-17 20:39:03 -0800

    adjusting data and app

--- app.R
+++ app.R
@@ -3,77 +3,67 @@ library(tidyverse)
 library(shinydashboard)
 library(plotly)
 library(DT)
-#setwd("~/Library/Mobile Documents/com~apple~CloudDocs/School/Grad/CCIDM/Shiny Workshop/Workshop_Test")
 
 post_data.df<-read_csv("data/Post Data.csv")%>%
   mutate(across(c("Date", "Revenue"), ~ str_extract(.x, "\\d+")))%>%
   mutate(across(c("Date", "Revenue"), as.numeric))%>%
-  mutate(`Post Type`=factor(`Post Type`, levels=c("Featured Product","Announcement","Event Showcase","Employee of the Week")))%>%
+  mutate(Post_Type=factor(Post_Type, levels=c("Featured Product","Announcement","Event Showcase","Employee of the Week")))%>%
   arrange(Date)
-
 
 
 ui <- dashboardPage(
   dashboardHeader(title = "Social Media Posts"),
   dashboardSidebar(
     sidebarMenu(
-      selectInput("post_type", "Post Type:", choices = NULL, multiple=TRUE),
-      selectInput("state", "State:", choices = NULL, multiple=TRUE), 
+      selectInput("post_type", "Post Type:", choices = unique(post_data.df$Post_Type), multiple=TRUE),
       menuItem("Monthly Conversion Metrics", tabName = "conversion", icon = icon("chart-bar")),
-      menuItem("Engagement Metrics", tabName = "funnel", icon = icon("chart-bar"))
-      #menuItem("Data Download", tabName = "download", icon = icon("download"))#DT table to view and download dataa?
+      menuItem("Data Download", tabName = "download", icon = icon("download"))
     )
   ),
   dashboardBody(
-    tags$head(
-      tags$style(HTML("
-         #funnelPlot { height: calc(100vh - 150px) !important; } /* Adjusts height relative to viewport */
-         #commentPlot { height: calc(100vh - 150px) !important; }
-      ")) #custom css to have plot fill dynamically
-    ),
-    tabItems(
-      tabItem(tabName = "funnel",
-              fluidRow(
-                box(
-                  title = "Average Post Interaction by State", status = "primary", solidHeader = TRUE, 
-                  collapsible = TRUE, width = 12,
-                  plotlyOutput("funnelPlot")  
-                )
-              ),
-              fluidRow(
-                box(
-                  title = "Comment Distribution by State", status = "primary", solidHeader = TRUE, 
-                  collapsible = TRUE, width = 12,
-                  plotlyOutput("commentPlot")  
-                )
-              )
-      ),
-      tabItem(tabName = "conversion",
-              fluidRow(
-                box(
-                  title = "Average Conversions by State(Conversions/Clicks)", status = "primary", solidHeader = TRUE, 
-                  collapsible = TRUE, width = 6,
-                  uiOutput("conversionBox")  
-                ),
-                box(
-                  title = "Top 5 Posts with Lowest Conversion Rate", 
-                  status = "primary", 
-                  solidHeader = TRUE, 
-                  collapsible = TRUE, 
-                  width = 6,
-                  DTOutput("lowestConversionTable")
-                )
-              ),
-              
-              fluidRow(
-                box(title = "Total Conversions per Day", 
+      tabItems(
+        tabItem(tabName = "conversion",
+                fluidRow(
+                  box(
+                    title = "Average Conversions by Post Type", status = "primary", solidHeader = TRUE, 
+                    collapsible = TRUE, width = 6,
+                    uiOutput("conversionBox")  
+                  ),
+                  box(
+                    title = "Top 5 Posts with Lowest Conversion Rate", 
                     status = "primary", 
                     solidHeader = TRUE, 
                     collapsible = TRUE, 
-                    width = 12,
-                    plotlyOutput("conversionsTimeSeries"))
-              )
-      )
+                    width = 6,
+                    DTOutput("lowestConversionTable")
+                  )
+                ),
+                
+                fluidRow(
+                  box(title = "Total Conversions per Day", 
+                      status = "primary", 
+                      solidHeader = TRUE, 
+                      collapsible = TRUE, 
+                      width = 12,
+                      plotlyOutput("conversionsTimeSeries"))
+                )
+        ),
+        
+        tabItem(tabName = "download",
+                fluidRow(
+                  box(
+                    title = "Average Post Interaction by State", status = "primary", solidHeader = TRUE, 
+                    collapsible = TRUE, width = 12,
+                    DTOutput("rawDataTable")  
+                  )
+                ),
+                fluidRow(
+                  div(
+                    class = "text-center",
+                    downloadButton("download_data", "Download Raw Data")
+                  )
+                )
+        )
     )
   )
 )
@@ -81,47 +71,24 @@ ui <- dashboardPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  observe({
-    updateSelectInput(session, "post_type", choices = unique(post_data.df$`Post Type`))
-    updateSelectInput(session, "state", choices = unique(post_data.df$State))
-  })
   
   filtered_df<-reactive({
-    req(input$post_type, input$state)  # Ensure all inputs are set
+    req(input$post_type)
     
     post_data.df %>%
-      filter(
-        `Post Type` %in% input$post_type,
-        State %in% input$state
-      )
-  })
-  
-  funnel_data <- reactive({
-    data <- filtered_df()
-    
-    data %>%
-      group_by(State, `Post Type`) %>%
-      summarise(
-        Reactions = mean(Reactions, na.rm = TRUE),
-        Comments = mean(Comments, na.rm = TRUE),
-        Shares = mean(Shares, na.rm = TRUE),
-        Clicks = mean(Clicks, na.rm = TRUE)
-      ) %>%
-      pivot_longer(cols = Reactions:Clicks, names_to = "Stage", values_to = "Count") %>%
-      mutate(Stage=factor(Stage, levels=c("Reactions","Comments","Shares","Clicks")))%>%
-      ungroup()
+      filter(Post_Type %in% input$post_type)
   })
   
   conversion_data<-reactive({
     filtered_df()%>%
-      group_by(State) %>%
+      group_by(Post_Type) %>%
       summarize(avg_conversion_rate = mean(Conversions / Clicks, na.rm = TRUE)) %>%
       ungroup()
   })
   
   conversions_by_day <- reactive({
     filtered_df() %>%
-      group_by(State, Date) %>%
+      group_by(Post_Type, Date) %>%
       summarize(total_conversions = sum(Conversions, na.rm = TRUE)) %>%
       arrange(Date)
   })
@@ -134,65 +101,17 @@ server <- function(input, output, session) {
       select(!Reactions:Shares)
   })
   
-  # Plot the funnel
-  output$funnelPlot <- renderPlotly({
-    
-    post_type_colors <- c(
-      "Featured Product" = "#4E79A7", 
-      "Employee of the Week" = "#F28E2B",   
-      "Event Showcase" = "#59A14F",  
-      "Announcement" = "#E15759"    
-    )
-    
-    plot<-ggplot(funnel_data(), aes(x = Stage, y = Count, fill=`Post Type`)) +
-      geom_col(position="stack") +
-      facet_wrap(~State)+
-      #coord_flip() +
-      labs(
-        x = "Interaction",
-        y = "Count"
-      ) +
-      theme_minimal(base_size = 14)+
-      theme(
-        legend.position = "bottom",
-        legend.title = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text.y=element_text(size=14),
-        axis.title.y=element_text(size=16)
-        )+
-      scale_fill_manual(values = post_type_colors)
-    
-    ggplotly(plot, tooltip = c("Stage", "Count", "Post Type"))
-  })
-  
-  output$commentPlot <- renderPlotly({
-    
-    plot<-ggplot(filtered_df(), aes(x = Clicks, fill=`Post Type`)) +
-      geom_histogram(stat="bin", binwidth = 500, color = "black") +
-      facet_wrap(~State)+
-      theme_minimal(base_size = 14)+
-      theme(
-        legend.position = "bottom",
-        legend.title = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text.y=element_text(size=14),
-        axis.title.y=element_text(size=16)
-      )
-    
-    ggplotly(plot, tooltip = c("Stage", "Count", "Post Type"))
-  })
-  
+
+  ### outputs ###
   output$conversionBox <- renderUI({
     valueBoxes <- lapply(1:nrow(conversion_data()), function(i) {
       valueBox(
-        subtitle = conversion_data()$State[i],
+        subtitle = conversion_data()$Post_Type[i],
         value = paste0(round(conversion_data()$avg_conversion_rate[i] * 100, 2), "%"),
         color = "blue",
         width = 3
       )
     })
-    #tagList(valueBoxes)
-    #do.call(fluidRow, valueBoxes)
   })
   
   output$lowestConversionTable <- renderDT({
@@ -202,10 +121,27 @@ server <- function(input, output, session) {
   })
   
   output$conversionsTimeSeries <- renderPlotly({
-    plot_ly(data = conversions_by_day(), x = ~Date, y = ~total_conversions, color = ~State, type = 'scatter', mode = 'lines')
+    print(conversions_by_day())
+    plot_ly(data = conversions_by_day(), x = ~Date, y = ~total_conversions, color = ~Post_Type, type = 'scatter', mode = 'lines')
+  })
+  
+  
+  
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste("raw_data", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered_df(), file, row.names = FALSE)
+    }
+  )
+  
+  output$rawDataTable <- renderDT({
+    datatable(filtered_df(), 
+              options = list(pageLength = 5, autoWidth = FALSE),
+              rownames = FALSE)
   })
   
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
